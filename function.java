5de6bc650e79f17d8427db6912c469187eb40331
rjrishav09 <crazyrishavhacker@gmail.com>
2024-01-24 06:46:43 +0530

    1

--- function.java
+++ function.java
@@ -139,21 +139,6 @@ class Solution1 {
 }
 
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int pseudoPalindromicPaths(TreeNode root) {
         return dfs(root, 0);
