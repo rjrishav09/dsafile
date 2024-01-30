f164325d51e9e83dbdc94bb8b741a5635c4d4230
rjrishav09 <crazyrishavhacker@gmail.com>
2024-01-30 11:38:02 +0530

    1

--- function.java
+++ function.java
@@ -303,4 +303,32 @@ class MyQueue1 {
         return s1.isEmpty();
     }
 }
+class Solution13 {
+    public int evalRPN(String[] tokens) {
+        int[] stack = new int[tokens.length];
+        int top = 0;
+        for(String s : tokens) {
+            char c = s.charAt(0);
+            if(c=='+') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a+b;
+            } else if(c=='-' && s.length()==1) {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a-b;
+            } else if(c=='*') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a*b;
+            } else if(c=='/') {
+                int b = stack[--top];
+                int a = stack[--top];
+                stack[top++] = a/b;
+            } else 
+                stack[top++] = Integer.parseInt(s);
+        }
+        return stack[0];
+    }
+}
 
